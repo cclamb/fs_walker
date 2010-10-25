a9f6f9731753cff879e20ed8b90ce61f3f9ef49e
Robert A. Ballance <raballa@sandia.gov>
2010-10-25 08:41:37 -0600

    Many revisions, and for some reason it does not quite work!

--- src/visitor_callback.erl
+++ src/visitor_callback.erl
@@ -13,8 +13,9 @@
 %%% -- in this example, the state contains the file name and an IO handle.
 %%%
 init(Index, MaxClients) ->
+    io:format("Initializing client ~w of ~w~n", [Index, MaxClients]),
     FileName = lists:concat(["client.", Index, ".", MaxClients, ".out"]),
-    case file:open(FileName, [read, write]) of
+    case file:open(FileName, [append]) of
         {ok, IoDev} ->
             State = #state{rank=Index, io_dev=IoDev, filename=FileName},
             {ok, State};
